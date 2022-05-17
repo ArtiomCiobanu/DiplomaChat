ed33fe034ec21dd641f65dcc6f3aaaf710d759f7
Artiom Ciobanu <aciobanumain@gmail.com>
2022-05-17 18:55:12 +0300

    Removed RootNamespace

---
+++ DiplomaChat.Common/DiplomaChat.Common.Infrastructure/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DiplomaChat.Common.Infrastructure.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T?> ToNullable<T>(this IEnumerable<T> enumerable)
+        where T : struct
+    {
+        return enumerable.Select(value => new T?(value)).ToArray();
+    }
+}
---
+++ DiplomaChat.Common/DiplomaChat.Common.Infrastructure/Extensions/TaskExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace DiplomaChat.Common.Infrastructure.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async Task<TResult> ThenSelect<TResult, TTaskResult>(
+            this Task<TTaskResult> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var taskResult = await task;
+
+            return selector(taskResult);
+        }
+
+        public static async Task<IEnumerable<TResult>> ThenSelect<TResult, TTaskResult>(
+            this Task<IEnumerable<TTaskResult>> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var taskResult = await task;
+
+            return taskResult.Select(selector);
+        }
+
+        public static async Task<TResult[]> ThenSelect<TResult, TTaskResult>(
+            this Task<TTaskResult[]> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var taskResult = await task;
+
+            return taskResult.Select(selector).ToArray();
+        }
+
+        public static async Task<List<TResult>> ThenSelect<TResult, TTaskResult>(
+            this Task<List<TTaskResult>> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var taskResult = await task;
+
+            return taskResult.Select(selector).ToList();
+        }
+
+        public static async Task<TResult[]> ThenSelectArray<TResult, TTaskResult>(
+            this Task<IEnumerable<TTaskResult>> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var selected = await task.ThenSelect(selector);
+
+            return selected.ToArray();
+        }
+
+        public static async Task<List<TResult>> ThenSelectList<TResult, TTaskResult>(
+            this Task<IEnumerable<TTaskResult>> task,
+            Func<TTaskResult, TResult> selector)
+        {
+            var selected = await task.ThenSelect(selector);
+
+            return selected.ToList();
+        }
+    }
+}
