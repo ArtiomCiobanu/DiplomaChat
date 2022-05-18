2407454321c852fdffdac80dd8e829257b23f061
Artiom Ciobanu <aciobanumain@gmail.com>
2022-05-18 14:58:27 +0300

    Removed nullable context

--- DiplomaChat.Common/DiplomaChat.Common.Infrastructure.Logging/NotLoggedStores/Properties/NotLoggedPropertyInfo.cs
+++ DiplomaChat.Common/DiplomaChat.Common.Infrastructure.Logging/NotLoggedStores/Properties/NotLoggedPropertyInfo.cs
@@ -1,9 +1,4 @@
 ﻿namespace DiplomaChat.Common.Infrastructure.Logging.NotLoggedStores.Properties
 {
-    public class NotLoggedPropertyInfo
-    {
-        public string Name { get; init; }
-
-        public Type DeclaringType { get; init; }
-    }
+    public record class NotLoggedPropertyInfo(string Name, Type DeclaringType);
 }
